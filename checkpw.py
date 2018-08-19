827182d9d6681afca10f71174204ed4c85eaa0b4
Evan Longwood <evanmlongwood@gmail.com>
2018-08-18 21:00:33 -0700

    More conditionals.

--- checkpw.py
+++ checkpw.py
@@ -2,6 +2,10 @@ print('Enter your password.')
 typedPassword = input()
 if typedPassword == 'swordfish':
     print('Access Granted')
+elif typedPassword == 'mary':
+    print('Hint: The Password is a fish.')
+elif typedPassword == '12345':
+    print('That is really an obvious password.')
 else:
     print('Access Deniedtu')
 print('Done.')
