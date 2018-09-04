51df2429b327c79ecc2ae27e166c7480dcd30b6f
Evan <evanmlongwood@gmail.com>
2018-09-04 13:03:44 -0700

    Transposition Encryption Cipher.

---
+++ transpositionEncrypt.py
@@ -0,0 +1,43 @@
+# Transpositon Cipher Encryption
+# https://www.nostarch.com/crackingcodes/ (BSD Licensed)
+
+import pyperclip
+
+def main():
+    myMessage = 'Common sense is not so common.'
+    myKey = 8
+
+    ciphertext = encryptedMessage(myKey, myMessage)
+
+    # Print the encrypted string in ciphertext to the screen, with
+    # a | ("pipe" character) after it in case there are spaces at
+    # thee end of the encrypted message:
+    print(ciphertext + '|')
+
+    # Copy the encrypted string in cihertext tot the clipboard:
+    pyperclip.copy(ciphertext)
+
+def encryptedMessage(key, message):
+    # Each string in ciphertext represents a column in the grid:
+    ciphertext = [''] * key
+
+    # Loop through each column in ciphertext:
+    for column in range(key):
+        currentIndex = column
+
+        # Keep looping until currentIndex goes past the message length:
+        while currentIndex < len(message):
+            # Place the character at currentIndex in messsage at the
+            # end of the current column in the ciphertext list:
+            ciphertext[column] += message[currentIndex]
+
+            # move currentIndex over:
+            currentIndex += key
+
+    # Conver the ciphertext list into a single string value and return it:
+    return ''.join(ciphertext)
+
+# If transpositionEncrypt.py is run (instead of imp;orted as a module) call
+# the main() function:
+if __name__ == '__main__':
+    main()
